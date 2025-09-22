887b602643c057d2f8c38de156e0f820a02f97b9
dina zvyagina <krolik200211@gmail.com>
2025-09-22 09:29:05 +0300

    lesson1

--- урок 2, завдання 9.3.py
+++
@@ -1,3 +0,0 @@
-for i in range(1, 501):
-    if (i%12)==0 :
-        print(i)
--- урок2, завдання 9.4.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Введіть значення n: "))
-suma = 0
-for i in range(1, n+1):
-    suma += i**2
-print("Сума квадратів чисел від 1 до n: ",suma)
